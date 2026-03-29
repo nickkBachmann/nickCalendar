370885fcabd2fac2c275e053e07b2c28c55f9de9
nick <nbachmann2016@yahoo.com>
2026-03-29 13:57:24 -0500

    fixed first deploy error

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,13 @@ const isDev = process.env.NODE_ENV === 'development';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: isDev, // 👈 CLAVE
+  disable: isDev,
 });
 
 const nextConfig: NextConfig = {
   devIndicators: false,
   reactStrictMode: true,
+  turbopack: {}, // 👈 🔥 ESTO ARREGLA TODO
 };
 
 export default isDev ? nextConfig : withPWA(nextConfig);
