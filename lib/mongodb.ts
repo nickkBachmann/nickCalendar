32b10229e2f6c80b6e9082e5323f58bdf8566444
nick <nbachmann2016@yahoo.com>
2026-03-29 15:16:44 -0500

    ahorasicreoYApls

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,7 +5,10 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {
+  tls: true,
+  serverSelectionTimeoutMS: 5000,
+};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
